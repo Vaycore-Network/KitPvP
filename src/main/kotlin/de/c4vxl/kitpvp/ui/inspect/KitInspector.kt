a763531255978a44944dc9316739668f264d0c14
c4vxl <manage@c4vxl.de>
2026-04-10 18:32:16 +0200

    Added back button to KitInspector

--- src/main/kotlin/de/c4vxl/kitpvp/ui/inspect/KitInspector.kt
+++ src/main/kotlin/de/c4vxl/kitpvp/ui/inspect/KitInspector.kt
@@ -33,20 +33,8 @@ class KitInspector(
                 addMarginItems(0..17, 27..44, 18..18, 26..26)
                 addMarginItems(11..15, 29..33, 10..28 step 9, 16..34 step 9, material = Material.LIME_STAINED_GLASS_PANE)
 
-                setItem(16, ItemBuilder(
-                    Material.ITEM_FRAME,
-                    language.getCmp("inspector.item.about.name"),
-                    lore = buildList {
-                        repeat(3) { i ->
-                            add(language.getCmp(
-                                "inspector.item.about.lore.${i + 1}",
-                                kit.metadata.createdAt,
-                                kit.metadata.creatorPlayer.name ?: "/",
-                                kit.metadata.lastEdit
-                            ) as TextComponent)
-                        }
-                    }.toMutableList()
-                )
+                setItem(10, ItemBuilder(Material.RED_STAINED_GLASS_PANE, language.getCmp("inspector.item.back"))
+                    .guiItem { returnTo?.open() ?: player.inventory.close() }
                     .build())
 
                 addItem(item(Material.NAME_TAG, "rename") {
